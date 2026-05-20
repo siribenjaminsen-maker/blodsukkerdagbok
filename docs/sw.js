1d69ca7c4b7a55759b1da6dcde0dd064510e9f1e
siribenjaminsen-maker <siri.benjaminsen@gmail.com>
2026-05-20 22:03:56 +0200

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "blodsukkerdagbok-v3";
+const CACHE_NAME = "blodsukkerdagbok-v4";
 const APP_FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
