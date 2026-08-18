e3f8c36ef98e852716f981573b308e2b9df6b253
siribenjaminsen-maker <siri.benjaminsen@gmail.com>
2026-08-18 18:41:52 +0200

    Trigger GitHub Pages deploy

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "blodsukkerdagbok-v7";
+const CACHE_NAME = "blodsukkerdagbok-v8";
 const APP_FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
