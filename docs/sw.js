84a761bedfc3c620aa5f5a08402d0f9c85b4057f
siribenjaminsen-maker <siri.benjaminsen@gmail.com>
2026-08-24 18:06:49 +0200

    Refresh PWA cache after auth fix

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "blodsukkerdagbok-v8";
+const CACHE_NAME = "blodsukkerdagbok-v9";
 const APP_FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
